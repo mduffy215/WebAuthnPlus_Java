51fbbfd4a4ce3ffa79d6c0f656c54ec66a045cd4
mduff <mduff@Workstation>
2022-06-26 20:04:57 -0500

    Version update.

--- foundation/src/main/java/io/trustnexus/util/Constants.java
+++ foundation/src/main/java/io/trustnexus/util/Constants.java
@@ -15,6 +15,8 @@
 package io.trustnexus.util;
 
 public class Constants {
+	
+    public static final String VERSION = "0.0.1";
 
     // TODO: Change for your system.
     public static final String COOKIE_PATH = "/foundation/";
